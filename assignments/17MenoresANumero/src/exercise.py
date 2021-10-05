50e06f2fd47ad530f43948683ef0f0467ee1e661
AlexisLok <a01251886@itesm.mx>
2021-10-05 19:03:20 +0000

    Avance

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,7 +1,21 @@
+def matriz (renglon, column):
+    renglones = []
+    for r in range(renglon):
+        columnas = []
+        for c in range(column):
+            valores = int(input(" "))
+            columnas.append(valores)
+        renglones.append(columnas)
+    return (renglones)
 
+def matrizAux():
+    r = int(input(""))
+    c = int(input(""))
+    m = matriz (r,c) 
+    return m
 
 def main():
-   
+
 
 
 
