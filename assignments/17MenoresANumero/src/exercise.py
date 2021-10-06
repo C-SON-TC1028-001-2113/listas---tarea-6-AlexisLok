fe7a288375130a6f7267a73ac93f5217c8f4e26a
AlexisLok <a01251886@itesm.mx>
2021-10-06 04:37:24 +0000

    Ejercicio 2 de Tarea 6

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,22 +1,28 @@
-def matriz (renglon, column):
-    renglones = []
-    for r in range(renglon):
-        columnas = []
-        for c in range(column):
-            valores = int(input(" "))
-            columnas.append(valores)
-        renglones.append(columnas)
-    return (renglones)
+def matriz (): #Se define la funcon de creacion de matriz
+    matrizSub = [] #Se define la variable con lista vacia
 
-def matrizAux():
-    r = int(input(""))
-    c = int(input(""))
-    m = matriz (r,c) 
-    return m
+    #Se pide los parametros para construir matriz
+    renglon = int(input(""))
+    columna = int(input(""))
+    for r in range(renglon): #Se crea un ciclo con otro ciclo dentro para construir la lista de listas (matriz)
+        renglones = []
+        for c in range(columna):
+            valores = int(input(""))
+            renglones.append(valores)
+        matrizSub.append(renglones)
+    return matrizSub #Regresa la matriz ya construida
+    
+def menor(): #Se define la funcion que analizara la matriz construida y pondra todos los valores menores de 10 en una lista
+    MatrizMenor = []
+    arreglo = matriz()
+    for x in range(len(arreglo)): #Se crea un ciclo for con otro ciclo for para poder analizar cada valor de cada lista 
+        for y in range(len(arreglo[0])):
+            if arreglo[x][y] < 10: #Checa si el valor es menor a 10
+                MatrizMenor.append(arreglo[x][y]) 
+    return MatrizMenor #Regresa la matriz con solo valores menores a 10
 
 def main():
-
-
+    print(menor()) #Se imprime la matriz construida de valores menores a 10
 
 
 if __name__=='__main__':
